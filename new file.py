044e872a25d7f9af8f08e0cbee5b30fb471c886b
quicknightbird <43297478+quicknightbird@users.noreply.github.com>
2021-12-05 22:31:19 +0300

    add hashtaf
    add new hastag

--- new file.py
+++ new file.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 import math
 from selenium.webdriver.support.ui import Select
 import os
-
+#dasd
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
